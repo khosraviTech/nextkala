a6cd94d13672d40d0495077a87edee1bfd2a8de9
khosraviTech <khosravitech@gmail.com>
2026-05-29 08:20:48 +0330

    solve destructuring handlers in route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,10 @@
 import { handlers } from "@/auth"
+import * as authModule from "@/auth";
+console.log('Auth module:', authModule);
 
-export const { GET, POST } = handlers
+
+export const GET = handlers.GET;
+export const POST = handlers.POST;
 // ↑ This creates automatic API routes
 // POST /api/auth/callback/credentials → handles login
 // GET  /api/auth/session              → returns current user
