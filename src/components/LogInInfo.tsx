5dbc3dcb8016720d4cfbb6231325955385f97481
khosraviTech <khosravitech@gmail.com>
2026-05-30 13:48:25 +0330

    add logout

--- src/components/LogInInfo.tsx
+++ src/components/LogInInfo.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function LogInInfo() {
   const { data: session } = useSession();
 
   if (session?.user) {
-    return <div>👤 {session.user.name}</div>;
+    return (
+      <>
+        <div>👤 {session.user.name}</div>;
+        <button onClick={() => signOut()}>Log Out</button>
+      </>
+    );
   } else {
     return <Link href="/login">Log In</Link>;
   }
