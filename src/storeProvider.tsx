517645b25fad73b9ef440b2cf0cd17aad9271c77
khosraviTech <khosravitech@gmail.com>
2026-06-03 08:06:01 +0330

    storeProvider fixed.

--- src/storeProvider.tsx
+++ src/storeProvider.tsx
@@ -1,14 +1,15 @@
-// app/StoreProvider.tsx
 'use client';
-import { useRef } from 'react';
-import { Provider } from 'react-redux';
-import { makeStore, AppStore } from '@/lib/redux/store';
 
-export default function StoreProvider({ children }: { children: React.ReactNode }) {
-  const storeRef = useRef<AppStore>(null);
-  if (!storeRef.current) {
-    // Create the store instance the first time this renders
-    storeRef.current = makeStore();
-  }
-  return <Provider store={storeRef.current}>{children}</Provider>;
+import { useMemo } from 'react';
+import { Provider } from 'react-redux';
+import { makeStore } from '@/lib/redux/store';
+
+export default function StoreProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const store = useMemo(() => makeStore(), []);
+
+  return <Provider store={store}>{children}</Provider>;
 }
