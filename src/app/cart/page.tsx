8c4dfc5b1cce61b030baf919d48ac7e73484d817
khosraviTech <khosravitech@gmail.com>
2026-06-20 19:20:36 +0330

    add "Add to cart" btn onclick.

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,9 +1,74 @@
-import React from "react";
+"use client";
+
+import cartSlice from "@/lib/redux/features/cartSlice";
+import { useAppDispatch, useAppSelector } from "@/lib/redux/hooks";
+
+import Image from "next/image";
 
 export default function Cart() {
-  return <>
-  
-  {/* products */}
-  {/* order summery */}
-  </>;
+  const dispatch = useAppDispatch();
+  const cartItems = useAppSelector((state) => state.cart);
+  const { setQuantity } = cartSlice.actions;
+
+  return (
+    <>
+      <div>
+        {/* products */}
+        <div>
+          {cartItems.map((item) => (
+            <div key={item.id}>
+              {/* item image part */}
+              <Image
+                src={item.image[0]}
+                width={100}
+                height={100}
+                alt="Picture of Amir Mohammad Khosravi"
+                className="w-full h-full object-cover"
+              />
+              {/* item title part */}
+              <h1>{item.title}</h1>
+              {/* item price part */}
+              <h1>{item.price}</h1>
+              {/* quantity part */}
+              <div>
+
+                <button
+                  onClick={() =>
+                    dispatch(
+                      setQuantity({
+                        id: item.id,
+                        quantity: item.quantity - 1,
+                      }),
+                    )
+                  }
+                >
+                  -
+                </button>
+                <h2>{item.quantity}</h2>
+                <button
+                  onClick={() =>
+                    dispatch(
+                      setQuantity({
+                        id: item.id,
+                        quantity: item.quantity + 1,
+                      }),
+                    )
+                  }
+                >
+                  +
+                </button>
+
+              </div>
+
+              {/* total item price part */}
+
+              {item.totalItemPrice}
+            </div>
+          ))}
+        </div>
+
+        {/* order summery */}
+      </div>
+    </>
+  );
 }
