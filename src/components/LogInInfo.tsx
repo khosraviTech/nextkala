8d8639b169341139152c2c4fbcb9a58c8dd036aa
khosraviTech <khosravitech@gmail.com>
2026-06-24 17:11:41 +0330

    add DropdownMenu.

--- src/components/LogInInfo.tsx
+++ src/components/LogInInfo.tsx
@@ -1,18 +1,34 @@
 "use client";
 
-import { useAppDispatch, useAppSelector } from "@/lib/redux/hooks";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 
 export default function LogInInfo() {
-  
   const { data: session } = useSession();
 
   if (session?.user) {
     return (
       <>
-        <div>👤 {session.user.name}</div>
-        <button onClick={() => signOut()}>Log Out</button>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger asChild>
+            <button className="w-10 h-10 rounded-full">
+              {session.user.name}
+            </button>
+          </DropdownMenu.Trigger>
+
+          <DropdownMenu.Portal>
+            <DropdownMenu.Content>
+              <DropdownMenu.Item>profile</DropdownMenu.Item>
+
+              <DropdownMenu.Item>setting</DropdownMenu.Item>
+
+              <DropdownMenu.Item>orders</DropdownMenu.Item>
+
+              <DropdownMenu.Item>wishlist</DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Portal>
+        </DropdownMenu.Root>
       </>
     );
   } else {
