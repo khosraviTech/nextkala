aca2d61830fc4bfa6ed42b8afa123b6523301909
khosraviTech <khosravitech@gmail.com>
2026-06-30 08:49:11 +0330

    add  tailwind css to CreateOrder component.

--- src/app/order/orderComponents/CreateOrder.tsx
+++ src/app/order/orderComponents/CreateOrder.tsx
@@ -6,6 +6,7 @@ import { useSession } from "next-auth/react";
 import orederSlice from "@/lib/redux/features/orderSlice";
 import cartSlice from "@/lib/redux/features/cartSlice";
 import { useEffect, useMemo } from "react";
+import { ClipboardCheck } from "lucide-react";
 
 export default function CreateOrder() {
   const dispatch = useAppDispatch();
@@ -46,34 +47,65 @@ export default function CreateOrder() {
 
   return (
     <>
-      <div>
-        <div>
-          <h1>Thank you for your order!</h1>
-          <h3>your order has been confirmed.</h3>
+      <div className="grid grid-cols-12 gap-6 p-6">
+        {/* title of page */}
+        <div className="col-span-12 rounded-4xl pt-6 text-center">
+          <div className="inline-flex items-center gap-2 text-4xl">
+            <ClipboardCheck className="scale-140 text-accent-green shrink-0" />
+            <h1 className="text-primary  ">Thank you for your order!</h1>
+          </div>
+
+          <h3 className="text-secondary mt-2 text-xl">
+            your order has been confirmed.
+          </h3>
         </div>
-        <div>
-          <h4>Order number</h4>
-          <h4>{newOrder.orderId}</h4>
-          <h4>Date</h4>
-          <h4>{newOrder.createdAt.split("T")[0]}</h4>{" "}
-          <div>
+
+        {/* checkout  */}
+        <div className="grid grid-cols-12 gap-6 p-6 col-span-12 p-3 border-border border-2 rounded-3xl bg-bg-card">
+          <div className="col-span-12 flex justify-between text-2xl">
+            <h4 className="text-primary">Order number</h4>
+            <h4 className="text-accent-green ">{newOrder.orderId}</h4>
+          </div>
+
+          <div className="col-span-12 flex justify-between text-2xl">
+            <h4 className="text-primary">Date</h4>
+            <h4 className="text-accent-green ">{newOrder.createdAt.split("T")[0]}</h4>
+          </div>
+
+          <div className="col-span-12 flex justify-between text-2xl">
             {newOrder.items.map((item) => (
               <div key={item.title}>
-                <h4>
+                <h4 className="text-primary">
                   {item.title} x{item.quantity}
                 </h4>
-                <h4>${item.totalItemPrice}</h4>
+                <h4 className="text-accent-green ">${item.totalItemPrice}</h4>
               </div>
             ))}
           </div>
-          <h4>subtotal</h4>
-          <h4>${totalPrice}</h4>
-          <h4>shipping</h4>
-          <h4>$10</h4>
-          <h4>Tax</h4>
-          <h4>{newOrder.tax}</h4>
-          <h4>Total</h4>
-          <h4>${newOrder.total}</h4>
+
+
+          <div className="col-span-12 flex justify-between text-2xl">
+            <h4 className="text-primary">subtotal</h4>
+            <h4 className="text-accent-green ">${totalPrice}</h4>
+          </div>
+
+
+          <div className="col-span-12 flex justify-between text-2xl">
+            <h4 className="text-primary">shipping</h4>
+            <h4 className="text-accent-green ">$10</h4>
+          </div>
+
+
+          <div className="col-span-12 flex justify-between text-2xl">
+            <h4 className="text-primary">Tax</h4>
+            <h4 className="text-accent-green ">{newOrder.tax}</h4>
+          </div>
+
+
+          <div className="col-span-12 flex justify-between text-2xl">
+            <h4 className="text-primary">Total</h4>
+            <h4 className="text-accent-green ">${newOrder.total}</h4>
+          </div>
         </div>
       </div>
     </>
