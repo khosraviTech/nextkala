ac5e90d173bc866921974c860ca15eac4201a5dd
khosraviTech <khosravitech@gmail.com>
2026-07-03 15:40:37 +0330

    remove searchBar component.

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,42 +5,34 @@ import LogInInfo from "./LogInInfo";
 import { Heart, ShoppingCart } from "lucide-react";
 import SearchBar from "./SearchBar";
 
-
-
 export default function Menu() {
   return (
     <div className="flex items-center justify-between gap-6 p-2 bg-white w-full   shadow-lg outline-0 ">
-      
-      
       <div className="flex items-center gap-6">
-      
-      <Link href="/">
-        <Logo />
-      </Link>
-      <Link href="/">Home</Link>
+        <Link href="/">
+          <Logo />
+        </Link>
+        <Link href="/">Home</Link>
 
-      <Link href="/about">About</Link>
-      
-        <LogInInfo />
-     </div>
-
-      <div className="flex items-center gap-6">
-       {/* SearchBar */}
-      <SearchBar/>
-
-      {/* Cart Icon */}
-      <Link href="/cart">
-        <ShoppingCart />
-      </Link>
-
-      {/* Wishlist Icon */}
-      <Link href="/wishlist">
-        <Heart />
-      </Link>
+        <Link href="/about">About</Link>
       </div>
 
-     
+      <div className="flex items-center gap-6">
+        {/* SearchBar */}
+        {/* <SearchBar/> */}
 
+        {/* Login User */}
+        <LogInInfo />
+        {/* Cart Icon */}
+        <Link href="/cart">
+          <ShoppingCart />
+        </Link>
+
+        {/* Wishlist Icon */}
+        <Link href="/wishlist">
+          <Heart />
+        </Link>
+      </div>
     </div>
   );
 }
