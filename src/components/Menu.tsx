3d9bcd448d363fed45b20df9c7ff9ef6a2fa86f7
khosraviTech <khosravitech@gmail.com>
2026-07-04 11:36:37 +0330

    make header responsive

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -7,12 +7,12 @@ import CartItemCounter from "./CartItemCounter";
 
 export default function Menu() {
   return (
-    <div className="flex items-center justify-between gap-6 p-2 bg-white w-full   shadow-lg outline-0 ">
-      <div className="flex items-center gap-6">
-        <Link href="/">
+    <div className="flex items-center justify-between gap-6  p-2 bg-white w-full   shadow-lg outline-0 ">
+      <div className="flex items-center gap-6 max-sm:gap-2">
+        <Link href="/" >
           <Logo />
         </Link>
-        <Link href="/">Home</Link>
+        <Link href="/">Products</Link>
 
         <Link href="/about">About</Link>
       </div>
@@ -26,7 +26,7 @@ export default function Menu() {
 
         {/* Cart Icon */}
         <Link href="/cart" className="relative flex items-center">
-          <ShoppingCart className='scale-120 text-green-600' />
+          <ShoppingCart className='scale-120 text-green-600 max-sm:scale-100' />
 
           <div className="absolute -top-2 -right-1 ">
             <CartItemCounter  />
@@ -35,7 +35,7 @@ export default function Menu() {
 
         {/* Wishlist Icon */}
         <Link href="/wishlist">
-          <Heart className="text-pink-600 scale-150 mr-5" />
+          <Heart className="text-pink-600 scale-150 mr-5 max-sm:scale-100" />
         </Link>
       </div>
     </div>
