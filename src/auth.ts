759e2d75e42c982e291a36a590ecc6e8dcff9b66
khosraviTech <khosravitech@gmail.com>
2026-07-07 14:17:48 +0330

    update for build in vercel.

--- src/auth.ts
+++ src/auth.ts
@@ -66,7 +66,7 @@ export default NextAuth({
           };
         }
 
-
+return null
       },
     }),
 
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -5,43 +5,43 @@ declare module "next-auth" {
     interface Session {
         user: {
             id: string;
-            firstName: string;
-            lastName: string;
-            phone: string;
-            username: string;
-            gender: string;
-            age: number;
-            birthDate: string;
-            address: Address;
-            company: Company;
+            firstName?: string;
+            lastName?: string;
+            phone?: string;
+            username?: string;
+            gender?: string;
+            age?: number;
+            birthDate?: string;
+            address?: Address;
+            company?: Company;
         } & DefaultSession["user"];
     }
 
     interface User {
         id: string;
-        firstName: string;
-        lastName: string;
-        phone: string;
-        username: string;
-        gender: string;
-        age: number;
-        birthDate: string;
-        address: Address;
-        company: Company;
+        firstName?: string;
+        lastName?: string;
+        phone?: string;
+        username?: string;
+        gender?: string;
+        age?: number;
+        birthDate?: string;
+        address?: Address;
+        company?: Company;
     }
 }
 
 declare module "next-auth/jwt" {
     interface JWT {
         id: string;
-        firstName: string;
-        lastName: string;
-        phone: string;
-        username: string;
-        gender: string;
-        age: number;
-        birthDate: string;
-        address: Address;
-        company: Company;
+        firstName?: string;
+        lastName?: string;
+        phone?: string;
+        username?: string;
+        gender?: string;
+        age?: number;
+        birthDate?: string;
+        address?: Address;
+        company?: Company;
     }
 }
