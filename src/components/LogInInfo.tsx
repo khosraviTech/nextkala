cf41471f26701babadb9e8c38a3e2cdb505344ea
khosraviTech <khosravitech@gmail.com>
2026-09-15 08:57:57 +0330

    update metadata title and description.

--- src/components/LogInInfo.tsx
+++ src/components/LogInInfo.tsx
@@ -14,26 +14,23 @@ export default function LogInInfo() {
         <DropdownMenu.Root>
           <DropdownMenu.Trigger asChild>
             <button className="cursor-pointer flex items-center gap-2 ">
-              <div className="rounded-full overflow-hidden border-3 border-black shadow-lg" >
+              <div className="rounded-full overflow-hidden border-3 border-black shadow-lg">
                 <Image
                   src={session.user.image!.toString()}
                   alt={`${session.user.name} image`}
                   width={30}
                   height={30}
-                  // TODO: fix image hight when logged in with gmail.
                   className="w-7.5 h-7.5"
-                  
                 />
               </div>
               {session.user.name}
             </button>
           </DropdownMenu.Trigger>
 
-          <DropdownMenu.Portal >
+          <DropdownMenu.Portal>
             <DropdownMenu.Content className="grid gap-3  text-primary border rounded-b-3xl p-6 bg-bg-dark">
-
               <DropdownMenu.Item className="text-xl m-2 hover:border-none">
-                <Link href="/profile"  >profile</Link>
+                <Link href="/profile">profile</Link>
               </DropdownMenu.Item>
 
               <DropdownMenu.Item className="text-xl m-2 ">
