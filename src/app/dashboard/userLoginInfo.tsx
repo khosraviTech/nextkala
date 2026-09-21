f4fde05b908a68a631bd93b1f1204d71e4091d33
khosraviTech <khosravitech@gmail.com>
2026-09-21 10:34:00 +0330

    add userlogininfo component

---
+++ src/app/dashboard/userLoginInfo.tsx
@@ -0,0 +1,10 @@
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+export default function UserLoginInfo() {
+    const { data: session } = useSession();
+    if (session?.user) {
+        return (<>
+            <Link href="/dashboard">My analytics</Link>
+        </>)
+    }
+}
