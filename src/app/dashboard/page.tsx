fa7efb8ea9ae2833d1ef55541b18488ae5eee35c
khosraviTech <khosravitech@gmail.com>
2026-09-21 10:24:19 +0330

    add user dashboard welcom title.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { useSession } from "next-auth/react";
 export default function Dashboard() {
     const { data: session } = useSession()
     return (<>
+    {/* welcome title */}
         <div><h1>welcom,{session?.user.name}</h1></div>
         <div className="gird grid-cols-12">
             <div></div>
