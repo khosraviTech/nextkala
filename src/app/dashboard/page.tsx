b375bf35f7d4804fcaf257011866a9d77a760a42
khosraviTech <khosravitech@gmail.com>
2026-09-22 13:01:05 +0330

    add total spent text color

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -11,7 +11,7 @@ export default function Dashboard() {
         <div className="col-span-12 grid grid-cols-12 gap-3">
             <div className="col-span-3 grid grid-cols-3 bg-[#f3debd] border-2 p-3 rounded-xl">
                 <Wallet className="col-span-1 bg-[#a18464] rounded  scale-190 m-3" />
-                <h6 className="text-gray">Total Spent</h6>
+                <h6 className="text-[#73685c]">Total Spent</h6>
 
             </div>
             <div className="col-span-3 bg-[#f3debd] border-2 p-3 rounded-xl"></div>
