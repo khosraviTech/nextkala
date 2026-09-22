7f3a3970b2e23626e840e1f8a2f357d4a20ca4e7
khosraviTech <khosravitech@gmail.com>
2026-09-22 11:02:18 +0330

    add welcome title style in dashboard.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,11 +2,11 @@
 import { useSession } from "next-auth/react";
 export default function Dashboard() {
     const { data: session } = useSession()
-    return (<>
-    {/* welcome title */}
-        <div><h1>welcom,{session?.user.name}</h1></div>
+    return (<div className="p-3">
+        {/* welcome title */}
+        <div ><h1 className="font-bold"><span className="text-[#c9a57d] font-bold"> WELCOME BACK, </span>{session?.user.name?.toUpperCase()} !</h1></div>
         <div className="gird grid-cols-12">
             <div></div>
         </div>
-    </>)
+    </div>)
 }
--- src/app/dashboard/userLoginInfo.tsx
+++ src/app/dashboard/userLoginInfo.tsx
@@ -1,10 +1,15 @@
 import { useSession } from "next-auth/react";
 import Link from "next/link";
+
 export default function UserLoginInfo() {
     const { data: session } = useSession();
     if (session?.user) {
         return (<>
+
             <Link href="/dashboard">My analytics</Link>
+
+
+
         </>)
     }
 }
